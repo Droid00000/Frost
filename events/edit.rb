ae4485887c000f9eacded0d701145c373a5072d6
Droid000 <johnship2876@gmail.com>
2024-09-28 23:58:26 -0400

    Rubocop fixes.

--- events/edit.rb
+++ events/edit.rb
@@ -21,12 +21,13 @@ def edit_event_role(data)
     return
   end
 
-  unless role = data.server.roles.find {|r| r.id == data.options['role']} && data.user.roles.include?(role)
+  unless role = data.server.roles.find { |r| r.id == data.options['role'] } && data.user.roles.include?(role)
     data.edit_response(content: RESPONSE[508])
     return
   end
 
-  modify_guild_role(data.server.id, data.user.id, name: data.options['name'], color: data.options['color'], type: event, role: data.options['role'])
+  modify_guild_role(data.server.id, data.user.id, name: data.options['name'], color: data.options['color'],
+                                                  type: event, role: data.options['role'])
 
   if !data.options['icon'].nil? && unlocked_icons?(data.server.boost_level)
     modify_guild_role(data.server.id, data.user.id, icon: data.options['icon'], type: event, role: data.options['role'])
