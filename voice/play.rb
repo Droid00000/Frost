f526a55a5d7aac08cc619e806e22546d08048a4f
DroidDevelopment <johnship2876@gmail.com>
2024-10-31 02:43:20 -0400

    Lay groundwork for music queue.

--- voice/play.rb
+++ voice/play.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'thread'
 require 'discordrb'
 require 'shellwords'
 require 'data/lavalink'
@@ -11,7 +12,9 @@ def voice_play(data)
     return
   end
 
+  embed_author = data.bot.voice(data.server.id) ? "Queued" : (embed_author || "Now Playing")
   track = LAVALINK.resolve(data.options['url'])
+  queue = Queue.new; queue << track
 
   data.edit_response do |builder|
     builder.add_embed do |embed|
@@ -20,12 +23,15 @@ def voice_play(data)
       embed.url = track.source
       embed.description = "**Duration:** `#{track.duration}`"
       embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: track.cover)
-      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Now Playing")
-      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Requested by #{data.user.display_name}",
-                                                          icon_url: data.user.avatar_url)
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: embed_author)
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Requested by #{data.user.display_name}", icon_url: data.user.avatar_url)
     end
   end
 
   data.bot.voice_connect(data.user.voice_channel)
-  data.bot.voice(data.server).play_io(IO.popen("yt-dlp -q -o - #{Shellwords.escape(track.playback)}"))
+
+  until queue.empty?
+    track = queue.pop
+    data.bot.voice(data.server).play_io(IO.popen("yt-dlp -q -o - #{Shellwords.escape(track.playback)}"))
+  end
 end
