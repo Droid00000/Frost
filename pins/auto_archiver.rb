7d435e4134fc63169239109d3280c2895f78ac95
DroidDevelopment <johnship2876@gmail.com>
2024-11-01 17:51:46 -0400

    Cleanup pin archiver functionality.

--- pins/auto_archiver.rb
+++ pins/auto_archiver.rb
@@ -12,27 +12,16 @@ module AutoPinArchiver
     pins = channel.pins
 
     if pins.count == 50 && archiver_records(server: event.data['guild_id']&.to_i, type: :check)
-      archive_channel = event.bot.channel.(archiver_records(server: event.data['guild_id']&.to_i, type: :get))
+      archive_channel = event.bot.channel(archiver_records(server: event.data['guild_id']&.to_i, type: :get))
       message = pins[1]
 
-      if message.attachments.any?
-        archive_channel.send_embed do |embed|
-          embed.colour = UI[2]
-          embed.description = message.content.to_s
-          embed.image = Discordrb::Webhooks::EmbedImage.new(url: message.attachments[0].url.to_s)
-          embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: message.author.display_name, url: message.link, icon_url: message.author.avatar_url)
-          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{message.id} • #{time_data(message.timestamp.to_s)}")
-          embed.add_field(name: 'Source', value: "[Jump!](#{message.link})")
-        end
-
-      else
-        archive_channel.send_embed do |embed|
-          embed.colour = UI[2]
-          embed.description = message.content.to_s
-          embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: message.author.display_name, url: message.link, icon_url: message.author.avatar_url)
-          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{message.id} • #{resolve_time(message.timestamp.to_s)}")
-          embed.add_field(name: 'Source', value: "[Jump!](#{message.link})")
-        end
+      archive_channel.send_embed do |embed|
+        embed.colour = UI[2]
+        embed.description = message.content.to_s
+        embed.image = Discordrb::Webhooks::EmbedImage.new(url: message.attachments[0].url.to_s) if message.attachments.any?
+        embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: message.author.display_name, url: message.link, icon_url: message.author.avatar_url)
+        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{message.id} • #{time_data(message.timestamp.to_s)}")
+        embed.add_field(name: 'Source', value: "[Jump!](#{message.link})")
       end
       message.unpin
     end
--- pins/manual_archiver.rb
+++ pins/manual_archiver.rb
@@ -15,24 +15,13 @@ module ManualPinArchiver
       archive_channel = event.bot.channel(archiver_records(server: event.server.id, type: :get))
       message = pins[1]
 
-      if message.attachments.any?
-        archive_channel.send_embed do |embed|
-          embed.colour = UI[2]
-          embed.description = message.content.to_s
-          embed.image = Discordrb::Webhooks::EmbedImage.new(url: message.attachments[0].url.to_s)
-          embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: message.author.display_name, url: message.link.to_s, icon_url: message.author.avatar_url)
-          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{message.id} • #{time_data(message.timestamp.to_s)}")
-          embed.add_field(name: 'Source', value: "[Jump!](#{message.link})")
-        end
-
-      else
-        archive_channel.send_embed do |embed|
-          embed.colour = UI[2]
-          embed.description = message.content.to_s
-          embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: message.author.display_name, url: message.link.to_s, icon_url: message.author.avatar_url)
-          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{message.id} • #{resolve_time(message.timestamp.to_s)}")
-          embed.add_field(name: 'Source', value: "[Jump!](#{message.link})")
-        end
+      archive_channel.send_embed do |embed|
+        embed.colour = UI[2]
+        embed.description = message.content.to_s
+        embed.image = Discordrb::Webhooks::EmbedImage.new(url: message.attachments[0].url.to_s) if message.attachments.any?
+        embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: message.author.display_name, url: message.link.to_s, icon_url: message.author.avatar_url)
+        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{message.id} • #{time_data(message.timestamp.to_s)}")
+        embed.add_field(name: 'Source', value: "[Jump!](#{message.link})")
       end
       message.unpin
       event.edit_response(content: "#{RESPONSE[20]} #{EMOJI[3]}")
