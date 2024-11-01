e0df1402ca1e552fdb921f1b728e15466e1251b7
DroidDevelopment <johnship2876@gmail.com>
2024-11-01 15:53:30 -0400

    Add queue for playing tracks.

--- voice/play.rb
+++ voice/play.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal: true
 
-require 'thread'
 require 'discordrb'
 require 'shellwords'
 require 'data/lavalink'
 require 'data/constants'
+
+class Tracks
+  # A Hash to store the current queue.
+  @@track_queue = {}
+
+  # @param queue [Queue]
+  # @param server [Integer]
+  def initialize(queue, server)
+    @@track_queue[server.to_s] = Queue.new if @@track_queue[server.to_s].nil?
+    @@track_queue[server.to_s] << queue
+  end
+
+  # Fetches the current queue from the cache.
+  # @return [Queue] The current queue for the server.
+  def self.queue(server)
+    @@track_queue[server.to_s]
+  end
+end
 
 def voice_play(data)
   if data.user.voice_channel.nil?
@@ -12,9 +29,14 @@ def voice_play(data)
     return
   end
 
-  embed_author = data.bot.voice(data.server.id) ? "Queued" : (embed_author || "Now Playing")
+  status = if data.bot.voice(data.server)&.playing?
+             'Queued'
+           else
+             'Now Playing'
+           end
+
   track = LAVALINK.resolve(data.options['url'])
-  queue = Queue.new; queue << track
+  Tracks.new(track.playback, data.server.id)
 
   data.edit_response do |builder|
     builder.add_embed do |embed|
@@ -22,16 +44,16 @@ def voice_play(data)
       embed.colour = UI[5]
       embed.url = track.source
       embed.description = "**Duration:** `#{track.duration}`"
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: status)
       embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: track.cover)
-      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: embed_author)
       embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Requested by #{data.user.display_name}", icon_url: data.user.avatar_url)
     end
   end
 
   data.bot.voice_connect(data.user.voice_channel)
 
-  until queue.empty?
-    track = queue.pop
-    data.bot.voice(data.server).play_io(IO.popen("yt-dlp -q -o - #{Shellwords.escape(track.playback)}"))
+  until Tracks.queue(data.server.id).empty?
+    sleep 1 while data.bot.voice(data.server).playing?
+    data.bot.voice(data.server).play_io(IO.popen("yt-dlp -q -o - #{Shellwords.escape(Tracks.queue(data.server.id).pop)}"))
   end
 end
