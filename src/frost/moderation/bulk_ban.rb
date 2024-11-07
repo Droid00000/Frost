96b9b9a4bf3a7de1fbee2c0595091d77c5142f5f
DroidDevelopment <johnship2876@gmail.com>
2024-11-06 22:49:17 -0500

    Fix bulk_ban safeguard.

--- src/frost/moderation/bulk_ban.rb
+++ src/frost/moderation/bulk_ban.rb
@@ -21,7 +21,7 @@ def bulk_ban(data)
   end
 
   filtered_members = data.options['members'].to_a.reject do |member|
-    data.bot.member(data.server, member).highest_role >= event.user.highest_role
+    data.bot.member(data.server, member).highest_role.position >= data.user.highest_role.position
   end
 
   if filtered_members.empty?
