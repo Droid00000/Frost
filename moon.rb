45d271d692f39f7f35eebce38f8ea69b516d7594
DroidDevelopment <johnship2876@gmail.com>
2024-11-13 22:37:48 -0500

    Add dictionary for phases of the moon.

--- moon.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'net/http'
-require 'json'
-
-URL = "http://api.farmsense.net/v1/moonphases/?d=#{Time.now.to_i}"
-
-puts JSON.parse((Net::HTTP.get(URI(URL))))[0]['Phase']
-
