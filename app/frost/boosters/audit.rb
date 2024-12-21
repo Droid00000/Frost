341f68b8f2ad0f7c062d2f5eabcbc785af76e240
DroidDevelopment <johnship2876@gmail.com>
2024-12-20 21:59:07 -0500

    Clarifiy namespace.

--- app/frost/boosters/audit.rb
+++ app/frost/boosters/audit.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rufus::Scheduler.new.cron '30 18 * * 5' do
-  Frost::Boosters::Members.fetch.each do |guild_member|
-    next if get_booster_status(guild_member[:guild_id], guild_member[:user_id])
+  Frost::Boosters::Members.fetch.each do |member|
+    next if get_booster_status(member[:guild_id], member[:user_id])
 
-    delete_guild_role(guild_member[:guild_id], guild_member[:role_id])
-    Frost::Boosters::Members.delete(guild_member[:guild_id], guild_member[:user_id])
+    delete_guild_role(member[:guild_id], member[:role_id])
+    Frost::Boosters::Members.delete(member[:guild_id], member[:user_id])
   end
 end
