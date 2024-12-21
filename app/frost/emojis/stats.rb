1af52f18ee3422abddacb33d2645057fbb1462cc
DroidDevelopment <johnship2876@gmail.com>
2024-12-20 19:27:55 -0500

    Minor changes.

--- app/frost/emojis/stats.rb
+++ app/frost/emojis/stats.rb
@@ -29,15 +29,13 @@ def stats_command(data)
   Frost::Emojis.bottom(data).map do |emoji|
     next unless data.bot.emoji(emoji[:emoji_id])
 
-    emojis[0] << { main: data.bot.emoji(emoji[:emoji_id]), count: emoji[:balance] }
+    emojis[1] << { main: data.bot.emoji(emoji[:emoji_id]), count: emoji[:balance] }
   end
 
-  emojis[0].map! do |emoji|
-    "#{emoji[:main].mention} — #{emoji[:main].name} **(#{emoji[:count]})**\n"
-  end
-
-  emojis[1].map! do |emoji|
-    "#{emoji[:main].mention} — #{emoji[:main].name} **(#{emoji[:count]})**\n"
+  emojis.each do |stats|
+    stats.map! do |emoji|
+      "#{emoji[:main].mention} — #{emoji[:main].name} **(#{emoji[:count]})**\n"
+    end
   end
 
   data.edit_response do |builder|
