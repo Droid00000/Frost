a27bc775b31202a6c3320ec0a19022e12be551a6
Droid <johnship2876@gmail.com>
2025-01-10 01:28:14 -0500

    Utilize increment operator over an array.

--- app/frost/moderation/delete.rb
+++ app/frost/moderation/delete.rb
@@ -6,11 +6,11 @@ def delete_messages(data)
     return
   end
 
-  messages = []
+  count = 0
 
   ([1] * data.options["amount"]).each_slice(100) do |chunk|
-    messages << data.channel.prune(chunk.sum)
+    count += data.channel.prune(chunk.sum)
   end
 
-  data.edit_response(content: format(RESPONSE[68], messages.sum))
+  data.edit_response(content: format(RESPONSE[68], count))
 end
