19513c3dd4276eccaea628499a2fa63b2a04f6b1
Droid <johnship2876@gmail.com>
2025-01-24 00:43:18 -0500

    Fix the check.

--- app/frost/boosters/edit.rb
+++ app/frost/boosters/edit.rb
@@ -3,11 +3,6 @@
 module Boosters
   # Command handler for /booster role edit.
   def self.edit(data)
-    if data.options.empty?
-      data.edit_response(content: "#{RESPONSE[2]} #{EMOJI[2]}")
-      return
-    end
-
     unless data.server.bot.permission?(:manage_roles)
       data.edit_response(content: RESPONSE[47])
       return
@@ -35,6 +30,11 @@ module Boosters
 
     if Frost::Boosters::Ban.user?(data)
       data.edit_response(content: RESPONSE[6])
+      return
+    end
+
+    if data.options.empty?
+      data.edit_response(content: "#{RESPONSE[2]} #{EMOJI[2]}")
       return
     end
 
