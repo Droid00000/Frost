ea0f72c525f4dd5a8d1fc6b09be97d94a4cb1afd
Droid <johnship2876@gmail.com>
2025-02-16 21:57:29 -0500

    Remove nested directory.

--- core.rb
+++ core.rb
@@ -11,17 +11,17 @@ require "tzinfo/data"
 require "unicode/emoji"
 require "rufus-scheduler"
 require "selenium-webdriver"
-require "app/frost/models/embeds"
-require "app/frost/models/constants"
-require "app/frost/models/functions"
-require "app/frost/models/paginator"
-require "app/frost/models/extensions"
-require "app/frost/pins/auto_archiver"
-require "app/frost/pins/manual_archiver"
+require "frost/models/embeds"
+require "frost/models/constants"
+require "frost/models/functions"
+require "frost/models/paginator"
+require "frost/models/extensions"
+require "frost/pins/auto_archiver"
+require "frost/pins/manual_archiver"
 
-Dir["app/frost/database/*.rb"].each { |file| require file }
+Dir["frost/database/*.rb"].each { |file| require file }
 
-Dir["app/frost/**/handler.rb"].each { |file| require file }
+Dir["frost/**/handler.rb"].each { |file| require file }
 
 @bot = Discordrb::Bot.new(token: CONFIG[:Discord][:TOKEN], intents: 34_443)
 
