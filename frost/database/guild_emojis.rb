611c249e522c8d9b16b7814d37c347e1e0e64661
Droid <johnship2876@gmail.com>
2025-02-17 00:57:10 -0500

    Emoji Cleanup

--- frost/database/guild_emojis.rb
+++ frost/database/guild_emojis.rb
@@ -6,13 +6,32 @@ module Frost
     # Easy way to access emojis.
     @@emojis = []
 
+    # Returns all emojis.
+    def self.drain; @@emojis; end
+
     # Easy way to access the DB.
     @@pg = POSTGRES[:emoji_tracker]
+
+    # Prune un-used emojis from the DB.
+    def self.prune(range)
+      range.to_a.each do |number|
+        POSTGRES.transaction do
+          @@pg.where(balance: number).delete
+        end
+      end
+    end
 
     # @param emoji [Discordrb::Emoji]
     # @param guild [Discordrb::Server]
     def initialize(emoji, guild)
       @@emojis << { emoji: emoji, guild: guild }
+    end
+
+    # @param data [Discordrb::Interaction]
+    def self.any?(data)
+      POSTGRES.transaction do
+        !@@pg.where(guild_id: data.server.id).empty?
+      end
     end
 
     # Insert a new emoji into the DB.
@@ -22,19 +41,11 @@ module Frost
       end
     end
 
-    # @param data [Discordrb::Interaction]
-    def self.any?(data)
-      !@@pg.where(guild_id: data.server.id).empty?
-    end
-
     # Returns the top 15 emojis.
     def self.top(data)
-      @@pg.where(guild_id: data.server.id).order(Sequel.desc(:balance)).limit(15)
-    end
-
-    # Returns all emojis.
-    def self.drain
-      @@emojis
+      POSTGRES.transaction do
+        @@pg.where(guild_id: data.server.id).order(Sequel.desc(:balance)).limit(15)
+      end
     end
   end
 end
--- frost/emojis/audit.rb
+++ frost/emojis/audit.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+Rufus::Scheduler.new.every "30d" do
+  Frost::Emojis.prune((1..2))
+end
+
 Rufus::Scheduler.new.cron "0 0 * * *" do
   while (emoji = Frost::Emojis.drain.shift)
     Frost::Emojis.add(emoji[:emoji], emoji[:guild])
