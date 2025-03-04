ebcee3e1b415d58d98bf4c52c6e1d02f6e521246
Droid <johnship2876@gmail.com>
2025-03-04 17:20:56 -0500

    Migration stuff for later.

--- frost/information/general/settings.rb
+++ frost/information/general/settings.rb
@@ -1,16 +1,51 @@
 # frozen_string_literal: true
 
-# An embed with data about a guild's enabled functionality.
-def general_settings(data)
-  data.edit_response do |builder|
-    builder.add_embed do |embed|
-      embed.title = EMBED[49]
-      embed.colour = UI[5]
-      embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: UI[1])
-      embed.add_field(name: EMBED[193], value: (EMBED[55] % Frost::Boosters::Settings.get(data))) if Frost::Boosters::Settings.get(data)
-      embed.add_field(name: EMBED[194], value: (EMBED[54] % Frost::Pins.get(data))) if Frost::Pins.get(data)
-      embed.add_field(name: EMBED[195], value: EMBED[56] % Frost::Roles.all(data).join(", ")) if Frost::Roles.enabled?(data)
-      embed.add_field(name: EMBED[253], value: EMBED[254] % Frost::Birthdays::Settings.role(data)) if Frost::Birthdays::Settings.role(data)
+module General
+  # An embed with data about a guild's enabled functionality.
+  def self.settings(data)
+    data.edit_response(new_components: true) do |_, builder|
+      # Create the main string with all the content we're planning on
+      # sending to Discord in the response body. This is seperated by
+      # newlines to achieve something that looks similar to embeds fields.
+      section_body = "#{RESPONSE[1]}\n\n#{RESPONSE[1]}\n\n#{RESPONSE[1]}"
+
+      # Create a container in order to simulate the old look and
+      # feel of an embed. I'm hoping there's some improvements to
+      # how this looks and feels on mobile, since it currently looks
+      # like hot garbage. Looks great on the mobile clients though!
+      builder.container do |container|
+        # Create a section to contain all of our main content, since
+        # if we attempt to only wrap our main heading text into a section
+        # we get some very weird spacing due to the fact that the thumbnail
+        # does not resize the spacing. This is a weird limitation that I hope
+        # gets addressed sometime in the future. For now, this will do though.
+        container.section do |section|
+          section.text_display(text: HEADER[1])
+          section.text_display(text: section_body)
+          section.thumbnail(media: data.server.bot.avatar_url)
+        end
+
+        # container.text_display(text: "**About Me**\nI was made by droid00000. My code is open source and can be viewed here!")
+
+        # container.text_display(text: "**Stats**\nI'm on 6 servers with a total of 62,344 members and 379 channels.")
+
+        # Add some spacing between the content of our container
+        # and the select menu that we're going to show the user.
+        container.seperator(divider: true, spacing: :small)
+
+        # Add a select menu for the enabled features a server has.
+        # We should avoid adding the select menu and the divider
+        # altogether if we don't have any enabled features in
+        # the current server we're operating on.
+        container.row do |row|
+          row.select_menu(custom_id: 'settings', placeholder: 'Pick a category...', min_values: 1) do |menu|
+            menu.option(label: 'Event Roles', value: 'Roles', description: 'Settings for custom server roles.', emoji: 1281715509750005831)
+            menu.option(label: 'Birthdays', value: 'Birthday', description: 'Settings for server birthdays.', emoji: 733787070123737109)
+            menu.option(label: 'Boosters', value: 'Boosters', description: 'Settings for server boosters.', emoji: 1320971944627146752)
+            menu.option(label: 'Pins', value: 'Pins', description: "Settings for the pin archiver.", emoji: 1320929329307324497)
+          end
+        end
+      end
     end
   end
 end
--- frost/information/help/index.rb
+++ frost/information/help/index.rb
@@ -2,22 +2,63 @@
 
 def help_index(data)
   data.send_message do |builder, components|
-    components.row do |menu|
-      menu.select_menu(custom_id: EMBED[126], placeholder: EMBED[65], min_values: 1) do |options|
-        options.option(label: EMBED[57], value: EMBED[81], description: EMBED[61], emoji: 1_320_928_322_804_388_003)
-        options.option(label: EMBED[238], value: EMBED[252], description: EMBED[239], emoji: 1_335_520_417_468_911_617)
-        options.option(label: EMBED[58], value: EMBED[84], description: EMBED[62], emoji: 1_320_971_944_627_146_752)
-        options.option(label: EMBED[60], value: EMBED[83], description: EMBED[64], emoji: 1_320_933_556_645_793_822)
-        options.option(label: EMBED[59], value: EMBED[82], description: EMBED[63], emoji: 1_320_929_329_307_324_497)
+    builder.add_embed do |embed|
+      embed.colour = UI[6]
+      embed.title = EMBED[68]
+      embed.timestamp = Time.now
+      embed.description = EMBED[69]
+      embed.add_field(name: EMBED[70], value: EMBED[71])
+      embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: UI[1])
+      embed.add_field(name: EMBED[196], value: format(EMBED[197], data.bot.count_servers, data.bot.count_members, data.bot.count_channels))
+    end
+  end
+end
 
-        builder.add_embed do |embed|
-          embed.colour = UI[6]
-          embed.title = EMBED[68]
-          embed.timestamp = Time.now
-          embed.description = EMBED[69]
-          embed.add_field(name: EMBED[70], value: EMBED[71])
-          embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: UI[1])
-          embed.add_field(name: EMBED[196], value: format(EMBED[197], data.bot.count_servers, data.bot.count_members, data.bot.count_channels))
+module Help
+  # An embed with data about a guild's enabled functionality.
+  def self.index(data)
+    data.edit_response(new_components: true) do |_, builder|
+      # Create the main string with all the content we're planning on
+      # sending to Discord in the response body. This is seperated by
+      # newlines to achieve something that looks similar to embeds fields.
+      section_body = "#{RESPONSE[1]}\n\n#{RESPONSE[1]}\n\n#{RESPONSE[1]}"
+
+      # Create a container in order to simulate the old look and
+      # feel of an embed. I'm hoping there's some improvements to
+      # how this looks and feels on mobile, since it currently looks
+      # like hot garbage. Looks great on the mobile clients though!
+      builder.container do |container|
+        # Create a section to contain all of our main content, since
+        # if we attempt to only wrap our main heading text into a section
+        # we get some very weird spacing due to the fact that the thumbnail
+        # does not resize the spacing. This is a weird limitation that I hope
+        # gets addressed sometime in the future. For now, this will do though.
+        container.section do |section|
+          section.text_display(text: HEADER[1])
+          section.text_display(text: section_body)
+          section.thumbnail(media: data.server.bot.avatar_url)
+        end
+
+        # container.text_display(text: "**About Me**\nI was made by droid00000. My code is open source and can be viewed here!")
+
+        # container.text_display(text: "**Stats**\nI'm on 6 servers with a total of 62,344 members and 379 channels.")
+
+        # Add some spacing between the content of our container
+        # and the select menu that we're going to show the user.
+        container.seperator(divider: true, spacing: :small)
+
+        # Add a select menu for the enabled features a server has.
+        # We should avoid adding the select menu and the divider
+        # altogether if we don't have any enabled features in
+        # the current server we're operating on.
+        container.row do |row|
+          row.select_menu(custom_id: EMBED[126], placeholder: EMBED[1], min_values: 1) do |menu|
+            menu.option(label: EMBED[1], value: EMBED[1], description: EMBED[1], emoji: 1281715509750005831)
+            menu.option(label: EMBED[1], value: EMBED[1], description: EMBED[1], emoji: 1281715509750005831)
+            menu.option(label: EMBED[1], value: EMBED[1], description: EMBED[1], emoji: 1281715509750005831)
+            menu.option(label: EMBED[1], value: EMBED[1], description: EMBED[1], emoji: 1281715509750005831)
+            menu.option(label: EMBED[1], value: EMBED[1], description: EMBED[1], emoji: 1281715509750005831)
+          end
         end
       end
     end
