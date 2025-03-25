c0a997a013749ac108ef66c099205144a942d2bc
Droid <johnship2876@gmail.com>
2025-03-25 10:28:31 -0400

    Update audit.rb

--- frost/expressions/audit.rb
+++ frost/expressions/audit.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-Rufus::Scheduler.new.cron "0 0 * * *" { Frost::Emojis.drain }
+Rufus::Scheduler.new.every "1d" { Frost::Emojis.drain }
 
 Rufus::Scheduler.new.every "30d" { Frost::Emojis.prune } 
