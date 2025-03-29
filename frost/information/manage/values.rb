5b5d5c42ebe3be5b8cc828c1b1453486128762a9
Droid <johnship2876@gmail.com>
2025-03-29 02:58:33 -0400

    Emoji

--- frost/information/manage/values.rb
+++ frost/information/manage/values.rb
@@ -6,7 +6,7 @@ module Settings
     1 => "These are the birthday settings for your server. Due to privacy reasons, the birthday announcement setting may not be respected. Birthday perks can be enabled, edited, and disabled with the </birthday admin:1354295411258429452> command group.",
     2 => "These are the enabled event roles for your server. Roles can be removed with the </component:1342292560378466346> command, and added with the  </component:1342292560378466346> command.",
     3 => "These are the booster settings for your server. Server boosters can be added, removed, banned, unbanned, and managed with the </booster admin:1354295411258429452> command group.",
-    4 => "**About Me**\nI was made by droid00000. My code is open source and can be viewed [here](https://github.com/Droid00000/Frost)!\n\n",
+    4 => "**About Me**\nI was made by droid00000. My code is open source and can be viewed [here](https://github.com/Droid00000/Frost)! <:Azaki_Wave:1354785417462943774>\n\n",
     5 => "**Manager**\nBirthday perks were enabled in this server by **%s** on <t:%s:D>\n\n",
     6 => "**Hoist Role**\nAll newly created roles will be moved above this role: <@&%s>\n\n",
     7 => "**Birthday Role**\nMembers will be given this role on their birthday: <@&%s>\n\n",
