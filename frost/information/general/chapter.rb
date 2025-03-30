e653fd4ba65c7b8d5deb80b6a9d4e1359446438a
Droid <johnship2876@gmail.com>
2025-03-30 17:05:52 -0400

    Waaaa

--- frost/information/general/chapter.rb
+++ frost/information/general/chapter.rb
@@ -8,17 +8,22 @@ module General
     time = Time.new(time.year, time.month, time.day, "11", "05")
     data.edit_response(content: format(RESPONSE[1], time.to_i))
   end
+
+  # Function to the find the date of the chapter.
+  def self.find_chapter
+    options = Selenium::WebDriver::Firefox::Options.new
+    options.add_argument("--headless")
+    driver = Selenium::WebDriver.for :firefox, options: options
+    driver.navigate.to CONFIG[:Chapter][:LINK]
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
+    wait.until { driver.find_element(:css, CONFIG[:Chapter][:ELEMENT]) }
+    time = Date.parse(driver.find_element(:css, CONFIG[:Chapter][:ELEMENT]).text)
+    name = "📖 #{time.strftime('%B')} #{time.day.ordinal} 3PM GMT"
+    Discordrb::API::Server.channel_name(CONFIG[:Discord][:TOKEN], CONFIG[:Chapter][:CHANNEL], name, REASON[2])
+  end
 end
 
 # Cron job to update the release channel.
-Rufus::Scheduler.new.cron "30 12 * * *" do
-  options = Selenium::WebDriver::Firefox::Options.new
-  options.add_argument("--headless")
-  driver = Selenium::WebDriver.for :firefox, options: options
-  driver.navigate.to CONFIG[:Chapter][:LINK]
-  wait = Selenium::WebDriver::Wait.new(timeout: 10)
-  wait.until { driver.find_element(:css, CONFIG[:Chapter][:ELEMENT]) }
-  time = Date.parse(driver.find_element(:css, CONFIG[:Chapter][:ELEMENT]).text)
-  name = "📖 #{time.strftime('%B')} #{time.day.ordinal} 3PM GMT"
-  Discordrb::API::Server.channel_name(@bot.token, CONFIG[:Chapter][:CHANNEL], name, REASON[2])
+Rufus::Scheduler.new.cron "05 12 * * *" do
+  General.find_chapter
 end
