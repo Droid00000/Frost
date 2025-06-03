bd0f702c6aa6706e8bf18b7e18e7536cd86f80d3
Droid <johnship2876@gmail.com>
2025-06-03 12:21:57 -0400

    Update ban.rb

--- frost/moderation/ban.rb
+++ frost/moderation/ban.rb
@@ -14,7 +14,7 @@ module Moderation
     end
 
     # The amount of time to delete messages for.
-    message_sec = data.options["messages"] * (86_400 || 0)
+    message_sec = (data.options["messages"] || 0) * 86_400
 
     # All the users that we want to ban, parsed by the bot.
     users = data.bot.parse_mentions(data.options["members"])
