3b6a37870a6c8ae1968ed12bedaa74027933c9f5
Droid <johnship2876@gmail.com>
2025-06-22 03:11:59 -0400

    remove ephemeral

--- frost/subscribers/handler.rb
+++ frost/subscribers/handler.rb
@@ -5,22 +5,22 @@ module BoosterCommands
 
   application_command(:booster).group(:role) do |group|
     group.subcommand(:claim) do |event|
-      event.defer(ephemeral: true)
+      event.defer(ephemeral: false)
       Boosters.create(event)
     end
 
     group.subcommand(:gradient) do |event|
-      event.defer(ephemeral: true)
+      event.defer(ephemeral: false)
       Boosters.colors(event)
     end
 
     group.subcommand(:delete) do |event|
-      event.defer(ephemeral: true)
+      event.defer(ephemeral: false)
       Boosters.delete(event)
     end
 
     group.subcommand(:edit) do |event|
-      event.defer(ephemeral: true)
+      event.defer(ephemeral: false)
       Boosters.edit(event)
     end
   end
