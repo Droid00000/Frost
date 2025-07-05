f9dfa262000e399d0f9a1b49b3e8330b0c4296f1
Droid <johnship2876@gmail.com>
2025-07-05 02:05:39 -0400

    sample

--- frost/information/vanity/values.rb
+++ frost/information/vanity/values.rb
@@ -13,6 +13,19 @@ module Vanity
     8 => "Please provide an end color for the gradient."
   }.freeze
 
+  # TODO: find a better solution later down the road.
+  def self.overwrite(data)
+    return false if data.user.owner? || data.user.roles.any? do |role|
+      role.permissions.bits.anybits?(268_435_464)
+    end
+
+    return true if data.channel.overwrites[data.user.id]&.allow&.bits&.anybits?(268_435_456)
+
+    data.user.roles.sort_by(&:permission).each do |role|
+      return true if data.channel.overwrites[role.id]&.allow&.bits&.anybits?(268_435_456)
+    end
+  end
+
   # Produce an audit reason string to show when operating on the current role.
   # @param interaction [Interaction] The current interaction the entry is for.
   # @return [String] A string that denotes the action type and current user ID.
